0805750ce6953131e03e6b591f9f6905ac1fd379
Zesjier <jkudler@uw.edu>
2020-12-10 23:00:48 -0800

    barebones website

--- wendingflats/settings.py
+++ wendingflats/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'nv.apps.NvConfig',
+    'ut.apps.UtConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- wendingflats/urls.py
+++ wendingflats/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
+    path('utah/', include('ut.urls')),
+    path('nevada/', include('nv.urls')),
     path('admin/', admin.site.urls),
 ]
