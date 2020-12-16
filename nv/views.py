da892f35dfc0b990002fe71574eaf06118bd7f29
RNW <rnw@Prospero>
2020-12-16 06:12:59 -0800

    Adds random casino dates and fixes secret css

--- nv/views.py
+++ nv/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from ut.views import base_context as ut_base_context
+import random
 
 def base_context(request):
     context = ut_base_context(request, 8, "Nevada")
@@ -13,7 +14,24 @@ def gallery(request):
     return render(request, 'nv/gallery.html', base_context(request))
 
 def games(request):
-    return render(request, 'nv/games.html', base_context(request))
+    context = base_context(request)
+    context['localtime'] = "7:00 PM"
+
+    rand = random.random()
+    date = 'June 11th, 2019'
+    if rand < 0.2:
+        date = 'June 7th, 2019'
+    elif rand < 0.4:
+        months = ['January', 'February', 'March', 'April', 'May', 'July', 'August', 'September', 'November', 'December']
+        date = '{} 11th, 2019'.format(random.choice(months))
+    elif rand < 0.6:
+        date = 'December 21st, 2012'
+    elif rand < 0.8:
+        date = 'June 11th, 1972'
+        context['past'] = 'True'
+    context['localdate'] = date
+
+    return render(request, 'nv/games.html', context)
 
 def rec(request):
     return render(request, 'nv/rec.html', base_context(request))
